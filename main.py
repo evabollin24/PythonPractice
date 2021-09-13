77361eaa09f84845d20af6573c707d7436f08f79
evabollin24 <817972@mystma.org>
2021-09-13 09:43:29 -0500

    Top 5 Movies

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("Star Wars")
 #print 2nd movie
 print("Pirates of the Caribbean")
 #print 3rd movie
-print("Spiderman")
+print("Spiderman movies")
 #print 4th movie
 print("Barbie movies lol")
 #print 5th movie
