5904466575d13a06d69038b6e713d36943888f3e
evabollin24 <817972@mystma.org>
2021-09-14 09:33:18 -0500

    Top Five Movies

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-print("Hello World!")
-#print 1st movie
-print("Star Wars")
-#print 2nd movie
-print("Pirates of the Caribbean")
-#print 3rd movie
-print("Spiderman movies")
-#print 4th movie
-print("Barbie movies lol")
-#print 5th movie
-print("Disney movies")
+def topFiveMovies():
+    #print 1st movie
+    print("Star Wars")
+
+    #print 2nd movie
+    print("Pirates of the Caribbean")
+
+    #print 3rd movie
+    print("Spiderman movies")
+
+    #print 4th movie
+    print("Barbie movies lol")
+
+    #print 5th movie
+    print("Disney Movies")
+print ("These are my top 5 movies:")
+topFiveMovies()
