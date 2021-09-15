781b7aec2d550945d2fd77f63a97aa7b43b5fc83
evabollin24 <817972@mystma.org>
2021-09-15 09:12:39 -0500

    5 Functions

--- main.py
+++ main.py
@@ -16,3 +16,79 @@ def topFiveMovies():
     print("Disney Movies")
 print ("These are my top 5 movies:")
 topFiveMovies()
+
+#define function
+def topFiveSports():
+    #print 1st sport
+    print("Cheerleading")
+
+    #print 2nd sport
+    print("Gymnastics")
+
+    #print 3rd sport
+    print("Dance")
+
+    #print 4th sport
+    print("Soccer")
+
+    #print 5th sport
+    print("Basketball")
+print ("These are my top 5 sports:")
+topFiveSports()
+
+#define function
+def topFiveColors():
+    #print 1st color
+    print("Blues")
+
+    #print 2nd color
+    print("Purples")
+
+    #print 3rd color
+    print("Yellow")
+
+    #print 4th color
+    print("Sage Green")
+
+    #print 5th color
+    print("Orange")
+print ("These are my top 5 colors:")
+topFiveColors()
+
+#define function
+def topFiveFoods():
+    #print 1st food
+    print("Tacos")
+
+    #print 2nd food
+    print("French Fries")
+
+    #print 3rd food
+    print("Macaroons")
+
+    #print 4th food
+    print("Ice Cream")
+
+    #print 5th food
+    print("Pasta")
+print ("These are my top 5 foods:")
+topFiveFoods()
+
+#define function
+def topFiveClasses():
+    #print 1st class
+    print("Math")
+
+    #print 2nd class
+    print("Video Production")
+
+    #print 3rd class
+    print("Photography")
+
+    #print 4th class
+    print("Spanish")
+
+    #print 5th class
+    print("Business Graphics")
+print ("These are my top 5 classes:")
+topFiveClasses()
