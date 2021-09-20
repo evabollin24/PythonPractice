36b69ab9ba640371056809ed144497407f46c741
evabollin24 <817972@mystma.org>
2021-09-20 09:14:13 -0500

    Return Functions

--- main.py
+++ main.py
@@ -1,149 +1,19 @@
 #define function
-def topFiveMovies():
-    #print 1st movie
-    print("Star Wars")
+def woof():
+    #return statment
+    return "woof"
 
-    #print 2nd movie
-    print("Pirates of the Caribbean")
+print (woof())
 
-    #print 3rd movie
-    print("Spiderman movies")
+def cat():
+    return "cat"
 
-    #print 4th movie
-    print("Barbie movies lol")
+def dog():
+    return "dog"
 
-    #print 5th movie
-    print("Disney Movies")
-print("These are my top 5 movies:")
-topFiveMovies()
+print(cat() + dog())
 
-#define function
-def topFiveSports():
-    #print 1st sport
-    print("Cheerleading")
+def planet():
+    print (8)
 
-    #print 2nd sport
-    print("Gymnastics")
-
-    #print 3rd sport
-    print("Dance")
-
-    #print 4th sport
-    print("Soccer")
-
-    #print 5th sport
-    print("Basketball")
-print("These are my top 5 sports:")
-topFiveSports()
-
-#define function
-def topFiveColors():
-    #print 1st color
-    print("Blues")
-
-    #print 2nd color
-    print("Purples")
-
-    #print 3rd color
-    print("Yellow")
-
-    #print 4th color
-    print("Sage Green")
-
-    #print 5th color
-    print("Orange")
-print("These are my top 5 colors:")
-topFiveColors()
-
-#define function
-def topFiveFoods():
-    #print 1st food
-    print("Tacos")
-
-    #print 2nd food
-    print("French Fries")
-
-    #print 3rd food
-    print("Macaroons")
-
-    #print 4th food
-    print("Ice Cream")
-
-    #print 5th food
-    print("Pasta")
-print("These are my top 5 foods:")
-topFiveFoods()
-
-#define function
-def topFiveClasses():
-    #print 1st class
-    print("Math")
-
-    #print 2nd class
-    print("Video Production")
-
-    #print 3rd class
-    print("Photography")
-
-    #print 4th class
-    print("Spanish")
-
-    #print 5th class
-    print("Business Graphics")
-print("These are my top 5 classes:")
-topFiveClasses()
-
-#define a function that outputs your name
-def myNameIs(turtle):
-
-    #output string
-    print(turtle + " is my name!")
-
-#run function
-myNameIs("Eva")
-
-#define function
-def timesTwo(panda):
-
-    #output
-    print(panda * 2)
-
-#run function
-timesTwo(8)
-
-#define function
-def plusFive(cow):
-
-    #output
-    print(cow + 5)
-
-#run function
-plusFive(8)
-
-#define function
-def dividedByTwo(horse):
-
-    #output
-    print(horse / 2)
-
-#run function
-dividedByTwo(8)
-
-#define function
-def addTwoNumbers(x, y):
-
-    #output
-    print(x + y)
-
-#run function
-addTwoNumbers(8, 8)
-
-#define function
-def madLib(adjective, place, placee, nounAnimal, adjectivee, nounFood, placeee, verb, adjectiveee):
-
-    #print mablib
-    print("I went on a " + adjective + " trip with my family this year. We usually go someplace that is near " + place + " or " + placee + 
-    ", but we decided to mix it up this year. One morning I got to ride a " + nounAnimal + ". That was " + adjectivee + "! Another day we got up early and ate " + nounFood + 
-    ", then we went to the " + placeee + " to " + verb + ". It was " + adjectiveee + "! I can't wait for our vacation next year!")
-
-madLib("elegant", "Flordia", "Russia", "panther", "sad", "pasta", "Mcdonalds", "jump", "depressing")
+print(planet())
