8a1b54d53befcdeeda072c801316b1012a94c01e
evabollin24 <817972@mystma.org>
2021-09-27 09:44:18 -0500

    Mini Dicitionary w/ lower

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 def define(word):
-    if word == "dictionary":
+    if word.lower() == "dictionary":
         return "a book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage."
-    elif word == "pneumonoultramicroscopicsilicovolcanoconiosis":
+    elif word.lower() == "pneumonoultramicroscopicsilicovolcanoconiosis":
         return " A lung disease caused by the inhalation of very fine silicate or quartz dust, causing inflammation in the lungs"
-    elif word == "trampoline":
+    elif word.lower() == "trampoline":
         return "a piece of equipment consisting of a strong fabric sheet connected by springs to a frame, used as a springboard and landing area in doing acrobatic or gymnastic exercises"
-    elif word == "pony":
+    elif word.lower() == "pony":
         return "a horse of a small breed, especially one whose height at the withers is below 14 hands 2 inches (58 inches)"
-    elif word == "movie":
+    elif word.lower() == "movie":
         return "a story or event recorded by a camera as a set of moving images and shown in a theater or on television; a motion picture"
-    elif word == "gobbledygook":
+    elif word.lower() == "gobbledygook":
         return "language that is meaningless or is made unintelligible by excessive use of abstruse technical terms; nonsense"
-    elif word == "life":
+    elif word.lower() == "life":
         return "the condition that distinguishes animals and plants from inorganic matter, including the capacity for growth, reproduction, functional activity, and continual change preceding death"
-    elif word == "death":
+    elif word.lower() == "death":
         return "the action or fact of dying or being killed; the end of the life of a person or organism"
-    elif word == "candy":
+    elif word.lower() == "candy":
         return "a sweet food made with sugar or other sweeteners, typically formed in small, shaped pieces and flavored with chocolate, fruit, or nuts"
-    elif word == "water":
+    elif word.lower() == "water":
         return "a colorless, transparent, odorless liquid that forms the seas, lakes, rivers, and rain and is the basis of the fluids of living organisms"
     else:
         "we cannot define this :("
