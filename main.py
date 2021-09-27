1cdc09d0ed450a6565775c72b6ca1115a13d90ff
evabollin24 <817972@mystma.org>
2021-09-27 09:13:04 -0500

    equalsTen

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def greaterThanTen(x):
-    if(x > 10):
-        return("x is greater than 10!  :)")
-    elif(x == 10):
-        return("x equals 10! :O")
+def equalsTen(x):
+    if (x == 10):
+        return "x equals 10!!!!"
+    elif (x > 10):
+        return "x is greater than 10"
     else:
-        return("x is not greater than 10! :(")
+        "x is less than 10..."
 
-print(greaterThanTen(7))
+print(equalsTen(10))
