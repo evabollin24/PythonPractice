e4ee16e381748f09da59fd90d9f13f8a94caa9c6
evabollin24 <817972@mystma.org>
2021-09-29 09:28:52 -0500

    More Boolean Operators

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def twoBigNumbers(x, y):
     else:
         return False
 print(twoBigNumbers(30, 40))
+
+def twoSmallNumbers(x, y):
+    if x < 10 or y < 10:
+        return True
+    else:
+        return False
+print(twoSmallNumbers(2, 3))
+
+def twoBigNumberss(x, y):
+    if x > 10 or not y > 10:
+        return True
+    else:
+        return False
+print(twoBigNumberss(30, 40))
