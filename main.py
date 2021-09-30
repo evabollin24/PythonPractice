f92da3bcb2ec7e19a1ee35beeaf71373ee298fea
evabollin24 <817972@mystma.org>
2021-09-30 09:41:54 -0500

    While Loopssss

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def function():
-
-    count = 5
-
-    while count <= 100:
-        print(count)
-        count = count + 5
+    count = 1
+    yeet = 1
+    while yeet <= 10:
+        while count <= 10:
+            print(count)
+            count = count + 1
+        yeet = yeet + 1
+        count = 1
 function()
