84d59add0a3ff391af12a5f9906f304740bbb2c9
evabollin24 <817972@mystma.org>
2021-10-01 09:31:32 -0500

    Array Practice

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def function():
-    count = 1
-    yeet = 1
-    while yeet <= 10:
-        while count <= 10:
-            print(count)
-            count = count + 1
-        yeet = yeet + 1
-        count = 1
-function()
+def lastLetter(w):
+    return w[len(w) - 1]
+print(lastLetter("potato"))
