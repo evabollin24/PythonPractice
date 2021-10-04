1552bd93ec0041d13bd53f0a3b1d81aedda0bebf
evabollin24 <817972@mystma.org>
2021-10-04 08:50:28 -0500

    While Loop

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def lastLetter(w):
-    return w[len(w) - 1]
-print(lastLetter("potato"))
+def spellingBee(w):
+    x = 0
+    while x < len(w):
+        print(w[x])
+        x = x + 1
+spellingBee("potato")
